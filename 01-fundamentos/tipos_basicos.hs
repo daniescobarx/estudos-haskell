a850ec3adc81643fc17aad91939902c8b7d4f14d
daniescobarx <daniela.mollo@sptech.school>
2025-08-19 23:12:41 -0300

    add

---
+++ 01-fundamentos/tipos_basicos.hs
@@ -0,0 +1,35 @@
+inteiro :: Int
+inteiro = 42
+
+inteiroGrande :: Integer
+inteiroGrande = 124124153467363
+
+float :: Float
+float = 3.14
+
+double :: Double
+double = 2.718281828459045
+
+verdadeiro :: Bool
+verdadeiro = True
+
+falso :: Bool
+falso = False
+
+char :: Char
+char = 'D'
+
+minhaString :: String
+minhaString = "Hi, Daniela!"
+
+-- Função que soma dois inteiros
+somaDois :: Int -> Int -> Int
+somaDois x y = x + y
+
+-- Função que concatena duas strings
+juntaStrings :: String -> String -> String
+juntaStrings s1 s2 = s1 ++ s2
+
+-- Função que verifica se um número é positivo
+ehPositivo :: Int -> Bool
+ehPositivo n = n > 0
