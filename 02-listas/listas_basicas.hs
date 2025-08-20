5c7537695605d90bbc72941e8a14708fc4607951
daniescobarx <daniela.mollo@sptech.school>
2025-08-20 20:26:06 -0300

    add listas int, strings

---
+++ 02-listas/listas_basicas.hs
@@ -0,0 +1,9 @@
+-- Listas em haskell são estruturas imutáveis que podem armazenar múltiplos valores do mesmo tipo.
+
+-- Exemplo de lista de inteiros
+numeros :: [Int]
+numeros = [1, 2, 3, 4, 5]
+
+-- Exemplo de lista de strings
+nomes :: [String]
+nomes = ["Daniela", "Paula", "Escobar"]
